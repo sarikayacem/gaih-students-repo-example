40219ac649ae8a0bbe6eeae567bac1eea80ae9b1
sarikayacem <79401289+sarikayacem@users.noreply.github.com>
2021-03-10 19:18:49 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,9 @@
-#Explain your work
+liste_tek = [1,3,5,7,9,11,15]
+liste_cift = [0,2,4,6,8,10,12,14]
 
-#Question 1
-for x in range(a):
-	print(a)
+liste = liste_tek + liste_cift
+
+liste2=[i*2 for i in liste]
+
+for i in liste2:
+    print("{} veri tipi: {}".format(i, type(i)))
