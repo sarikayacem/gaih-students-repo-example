7d83aba0f39081787d0b123add30265fc6cff468
sarikayacem <79401289+sarikayacem@users.noreply.github.com>
2021-03-12 11:12:11 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,29 @@
-#Explain your work
+print("Cv kayıt uygulamasına hoşgeldiniz")
+print("Kişi 1 için bilgileri giriniz!")
+person_1 = {"isim":input("isim: "),"Soyisim":input("Soyisim: "),"Yas: ":input("Yasınız: "),"Telefon No: ":input("Telefon No:"),"Adres: ":input("Adresiniz: ")}
+print("Kişi 2 için bilgileri giriniz!")
+person_2 = {"isim":input("isim: "),"Soyisim":input("Soyisim: "),"Yas: ":input("Yasınız: "),"Telefon No: ":input("Telefon No:"),"Adres: ":input("Adresiniz: ")}
+print("Kişi 3 için bilgileri giriniz!")
+person_3 = {"isim":input("isim: "),"Soyisim":input("Soyisim: "),"Yas: ":input("Yasınız: "),"Telefon No: ":input("Telefon No:"),"Adres: ":input("Adresiniz: ")}
+print("Kişi 4 için bilgileri giriniz!")
+person_4 = {"isim":input("isim: "),"Soyisim":input("Soyisim: "),"Yas: ":input("Yasınız: "),"Telefon No: ":input("Telefon No:"),"Adres: ":input("Adresiniz: ")}
+print("Kişi 5 için bilgileri giriniz!")
+person_5 = {"isim":input("isim: "),"Soyisim":input("Soyisim: "),"Yas: ":input("Yasınız: "),"Telefon No: ":input("Telefon No:"),"Adres: ":input("Adresiniz: ")}
+print("Kisi 1 icin cv bilgileri: ")
+for isim,kullanıcı in person_1.items():
+    print("{} --> {}".format(isim,kullanıcı))
+print("Kisi 2 icin cv bilgileri: ")
+for isim,kullanıcı in person_2.items():
+    print("{} --> {}".format(isim,kullanıcı))
+print("Kisi 3 icin cv bilgileri: ")
+for isim,kullanıcı in person_3.items():
+    print("{} --> {}".format(isim,kullanıcı))
+print("Kisi 4 icin cv bilgileri: ")
+for isim,kullanıcı in person_4.items():
+    print("{} --> {}".format(isim,kullanıcı))
+print("Kisi 5 icin cv bilgileri: ")
+for isim,kullanıcı in person_5.items():
+    print("{} --> {}".format(isim,kullanıcı))
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
